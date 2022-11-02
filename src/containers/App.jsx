c9e02a1fade72e32238ef1b6a8599645078f0ba6
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-11-02 14:38:23 -0700

    [UDPATE] add to formik of register with formik

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registrar from './Register';
+import Information from './Information';
 import AppContext from '../contexts/AppContext';
 import useInitialState from '../hooks/useInitialState';
 import './styles/App.css';
@@ -13,6 +14,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Registrar />} />
+          <Route path="/information" element={<Information />} />
         </Routes>
       </BrowserRouter>
     </AppContext.Provider>
