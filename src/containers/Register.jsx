d268ed1f69227dc6841f24e632ccd000ba44c498
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-11-03 13:37:46 -0700

    Poner numero de telefono con codigo de pais material ui

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -5,8 +5,11 @@ import React from 'react';
 import { TextField, MenuItem } from '@mui/material/';
 import { Form, useFormik, FormikProvider } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { Helmet } from 'react-helmet';
+import MuiPhoneNumber from 'material-ui-phone-number';
 import * as Yup from 'yup';
 import Header from '../components/Header';
+import Asynchronous from '../components/AsyncHronous';
 import './styles/App.css';
 
 export default function Registrar() {
@@ -23,7 +26,6 @@ export default function Registrar() {
     dateOfBirth: Yup.string().required('Fecha de nacimiento requerido'),
     gender: Yup.string().required('Género requerido'),
     maritalStatus: Yup.string().required('Estado civil requerido'),
-    mainPhone: Yup.string().required('Teléfono principal requerido'),
     phoneTypes: Yup.string().required('Tipo teléfono requerido'),
     mainMail: Yup.string().required('Correo principal requerido'),
     EmergencyPhone: Yup.string().required('Teléfono principal requerido'),
@@ -42,9 +44,8 @@ export default function Registrar() {
       dateOfBirth: '',
       gender: '',
       maritalStatus: '',
-      mainPhone: '',
-      phoneTypes: '',
       mainMail: '',
+      phoneTypes: '',
       EmergencyPhone: '',
       phone: '',
       emergencyContact: '',
@@ -75,161 +76,84 @@ export default function Registrar() {
     `${t('state.co')}`,
   ];
   const phoneType = [`${t('phone.tf')}`, `${t('phone.tm')}`];
-  const EmergencyPhoneType = ['None', 'United States + 1', 'Mexico + 52'];
-  const sgmType = ['Gnp', 'Axa', 'Metlife', 'Mediexcel', 'Privado', 'Chubb'];
+  // const sgmType = ['Gnp', 'Axa', 'Metlife', 'Mediexcel', 'Privado', 'Chubb'];
 
   return (
-    <section className="container">
-      <div className="contact-wrapper animated bounceInUp">
-        <div className="contact-form">
-          <nav>
-            <Header />
-          </nav>
-          <h3>{t('register.registrar')}</h3>
-          <FormikProvider value={formik}>
-            <Form noValidate autoComplete="off">
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('name')}
-                  error={Boolean(touched.name && errors.name)}
-                  helperText={touched.name && errors.name}
-                  id="outlined-basic"
-                  label={t('register.nombre')}
-                  variant="outlined"
-                  type="text"
-                />
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('firstName')}
-                  error={Boolean(touched.firstName && errors.firstName)}
-                  helperText={touched.firstName && errors.firstName}
-                  id="outlined-basic"
-                  label={t('register.apellidoPaterno')}
-                  variant="outlined"
-                  type="text"
-                />
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('lastName')}
-                  error={Boolean(touched.lastName && errors.lastName)}
-                  helperText={touched.lastName && errors.lastName}
-                  id="outlined-basic"
-                  label={t('register.apellidoMaterno')}
-                  variant="outlined"
-                  type="text"
-                />
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('dateOfBirth')}
-                  error={Boolean(touched.dateOfBirth && errors.dateOfBirth)}
-                  helperText={touched.dateOfBirth && errors.dateOfBirth}
-                  id="outlined-basic"
-                  variant="outlined"
-                  type="date"
-                />
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('gender')}
-                  error={Boolean(touched.gender && errors.gender)}
-                  helperText={touched.gender && errors.gender}
-                  id="gender"
-                  className="input"
-                  select
-                  label={t('register.genero')}
-                  variant="outlined"
-                >
-                  {genderTypes.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('maritalStatus')}
-                  error={Boolean(touched.maritalStatus && errors.maritalStatus)}
-                  helperText={touched.maritalStatus && errors.maritalStatus}
-                  id="maritalStatus"
-                  className="input"
-                  select
-                  label={t('register.estadoCivil')}
-                  variant="outlined"
-                >
-                  {maritalStatusType.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('mainPhone')}
-                  error={Boolean(touched.mainPhone && errors.mainPhone)}
-                  helperText={touched.mainPhone && errors.mainPhone}
-                  id="outlined-basic"
-                  label={t('register.telefonoPrincipal')}
-                  variant="outlined"
-                  type="tel"
-                />
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('phoneTypes')}
-                  error={Boolean(touched.phoneTypes && errors.phoneTypes)}
-                  helperText={touched.phoneTypes && errors.phoneTypes}
-                  id="phoneTypes"
-                  className="input"
-                  select
-                  label={t('register.tipoTelefono')}
-                  variant="outlined"
-                >
-                  {phoneType.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('mainMail')}
-                  error={Boolean(touched.mainMail && errors.mainMail)}
-                  helperText={touched.mainMail && errors.mainMail}
-                  id="outlined-basic"
-                  label={t('register.correoPrincipal')}
-                  variant="outlined"
-                  type="email"
-                />
-              </div>
-              <section>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Parte #1</title>
+        <meta name="description" content="Registrar Paciente" />
+      </Helmet>
+      <section className="container">
+        <div className="contact-wrapper animated bounceInUp">
+          <div className="contact-form">
+            <nav>
+              <Header />
+            </nav>
+            <h3>{t('register.registrar')}</h3>
+            <FormikProvider value={formik}>
+              <Form noValidate className="form-border" autoComplete="off">
                 <div>
                   <TextField
                     style={{ width: '230px' }}
-                    {...getFieldProps('EmergencyPhone')}
-                    error={Boolean(touched.EmergencyPhone && errors.EmergencyPhone)}
-                    helperText={touched.EmergencyPhone && errors.EmergencyPhone}
-                    id="EmergencyPhone"
+                    {...getFieldProps('name')}
+                    error={Boolean(touched.name && errors.name)}
+                    helperText={touched.name && errors.name}
+                    id="outlined-basic"
+                    label={t('register.nombre')}
+                    variant="outlined"
+                    type="text"
+                  />
+                </div>
+                <div>
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('firstName')}
+                    error={Boolean(touched.firstName && errors.firstName)}
+                    helperText={touched.firstName && errors.firstName}
+                    id="outlined-basic"
+                    label={t('register.apellidoPaterno')}
+                    variant="outlined"
+                    type="text"
+                  />
+                </div>
+                <div>
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('lastName')}
+                    error={Boolean(touched.lastName && errors.lastName)}
+                    helperText={touched.lastName && errors.lastName}
+                    id="outlined-basic"
+                    label={t('register.apellidoMaterno')}
+                    variant="outlined"
+                    type="text"
+                  />
+                </div>
+                <div>
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('dateOfBirth')}
+                    error={Boolean(touched.dateOfBirth && errors.dateOfBirth)}
+                    helperText={touched.dateOfBirth && errors.dateOfBirth}
+                    id="outlined-basic"
+                    variant="outlined"
+                    type="date"
+                  />
+                </div>
+                <div>
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('gender')}
+                    error={Boolean(touched.gender && errors.gender)}
+                    helperText={touched.gender && errors.gender}
+                    id="gender"
                     className="input"
                     select
-                    label={t('register.telefonoEmeergencia')}
+                    label={t('register.genero')}
                     variant="outlined"
                   >
-                    {EmergencyPhoneType.map((option) => (
+                    {genderTypes.map((option) => (
                       <MenuItem key={option} value={option}>
                         {option}
                       </MenuItem>
@@ -237,86 +161,97 @@ export default function Registrar() {
                   </TextField>
                 </div>
                 <div>
-                  {formik.values.EmergencyPhone === 'United States + 1' && (
-                    <div>
-                      <TextField
-                        style={{ width: '230px' }}
-                        {...getFieldProps('phone')}
-                        error={Boolean(touched.EmergencyPhone && errors.phone)}
-                        helperText={touched.EmergencyPhone && errors.phone}
-                        id="outlined-basic"
-                        placeholder="+ 1"
-                        variant="outlined"
-                        type="tel"
-                      />
-                    </div>
-                  )}
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('maritalStatus')}
+                    error={Boolean(touched.maritalStatus && errors.maritalStatus)}
+                    helperText={touched.maritalStatus && errors.maritalStatus}
+                    id="maritalStatus"
+                    className="input"
+                    select
+                    label={t('register.estadoCivil')}
+                    variant="outlined"
+                  >
+                    {maritalStatusType.map((option) => (
+                      <MenuItem key={option} value={option}>
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </TextField>
                 </div>
                 <div>
-                  {formik.values.EmergencyPhone === 'Mexico + 52' && (
-                    <div>
-                      <TextField
-                        style={{ width: '230px' }}
-                        {...getFieldProps('phone')}
-                        error={Boolean(touched.EmergencyPhone && errors.phone)}
-                        helperText={touched.EmergencyPhone && errors.phone}
-                        id="outlined-basic"
-                        placeholder="+ 52"
-                        variant="outlined"
-                        type="tel"
-                      />
-                    </div>
-                  )}
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('phoneTypes')}
+                    error={Boolean(touched.phoneTypes && errors.phoneTypes)}
+                    helperText={touched.phoneTypes && errors.phoneTypes}
+                    id="phoneTypes"
+                    className="input"
+                    select
+                    label={t('register.tipoTelefono')}
+                    variant="outlined"
+                  >
+                    {phoneType.map((option) => (
+                      <MenuItem key={option} value={option}>
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </TextField>
                 </div>
-
-              </section>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('emergencyContact')}
-                  error={Boolean(touched.emergencyContact && errors.emergencyContact)}
-                  helperText={touched.emergencyContact && errors.emergencyContact}
-                  id="outlined-basic"
-                  label={t('register.emergencyContact')}
-                  variant="outlined"
-                  type="email"
-                />
-              </div>
-              <div>
-                <TextField
-                  style={{ width: '230px' }}
-                  {...getFieldProps('sgm')}
-                  error={Boolean(touched.sgm && errors.sgm)}
-                  helperText={touched.sgm && errors.sgm}
-                  id="sgm"
-                  className="input"
-                  select
-                  label={t('register.sgm')}
-                  variant="outlined"
-                >
-                  {sgmType.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-              <div className="block">
-                <button type="button" onClick={prevHandleSubmit}>
-                  {t('saved.guardar')}
-                </button>
-              </div>
-            </Form>
-          </FormikProvider>
+                <div>
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('mainMail')}
+                    error={Boolean(touched.mainMail && errors.mainMail)}
+                    helperText={touched.mainMail && errors.mainMail}
+                    id="outlined-basic"
+                    label={t('register.correoPrincipal')}
+                    variant="outlined"
+                    type="email"
+                  />
+                </div>
+                <div>
+                  <MuiPhoneNumber
+                    {...getFieldProps('EmergencyPhone')}
+                    error={Boolean(touched.EmergencyPhone && errors.EmergencyPhone)}
+                    helperText={touched.EmergencyPhone && errors.EmergencyPhone}
+                    className="MuiPhoneNumber"
+                    defaultCountry="us"
+                    label={t('register.telefonoEmeergencia')}
+                  />
+                </div>
+                <div>
+                  <TextField
+                    style={{ width: '230px' }}
+                    {...getFieldProps('emergencyContact')}
+                    error={Boolean(touched.emergencyContact && errors.emergencyContact)}
+                    helperText={touched.emergencyContact && errors.emergencyContact}
+                    id="outlined-basic"
+                    label={t('register.emergencyContact')}
+                    variant="outlined"
+                    type="email"
+                  />
+                </div>
+                <div>
+                  <Asynchronous />
+                </div>
+                <div className="block">
+                  <button id="contact-form-form-button" type="button" onClick={prevHandleSubmit}>
+                    {t('saved.guardar')}
+                  </button>
+                </div>
+              </Form>
+            </FormikProvider>
+          </div>
+          <div className="contact-info fadeInDown">
+            <h4 className="contect-title">{t('footer.posibles')}</h4>
+            <p className="contect-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident ipsam
+              necessitatibus repellendus?
+            </p>
+          </div>
         </div>
-        <div className="contact-info fadeInDown">
-          <h4>{t('footer.posibles')}</h4>
-          <p className="contect-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident ipsam
-            necessitatibus repellendus?
-          </p>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
