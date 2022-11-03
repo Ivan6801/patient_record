d0761b343c358377535adb1b48956b7080a64bc9
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-11-03 13:59:33 -0700

    Dark in Light

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registrar from './Register';
 import Information from './Information';
-import AppContext from '../contexts/AppContext';
-import useInitialState from '../hooks/useInitialState';
+import ThemeContext from '../contexts/ThemeContext';
 import './styles/App.css';
 
 export default function App() {
-  const initialState = useInitialState();
+  const [theme, setTheme] = useState('bg-light');
 
   return (
-    <AppContext.Provider value={initialState}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Registrar />} />
-          <Route path="/information" element={<Information />} />
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
-
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`App ${theme}`}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Registrar />} />
+            <Route path="/information" element={<Information />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
   );
 }
