9c16211d3a94e5e5857bf90e50659851eebe5a7a
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-11-09 11:33:25 -0800

    Add to api people

--- src/components/ArticleUsers.jsx
+++ src/components/ArticleUsers.jsx
@@ -1,35 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 export default function ArticleUsers(props) {
   const { t } = useTranslation('global');
   const { item } = props;
   return (
-    <Link to="/search-patients">
-      <article key={item.id} className="card">
-        <img className="character-img" src={item.image} alt={item.name} />
-        <p className="character-info">
+    <article key={item.id} className="card">
+      <img className="character-img" src={item.picture.large} alt={item.name.title} />
+      <p className="character-info">
+        {' '}
+        <small className="character-name">
+          {t('register.nombre')}
           {' '}
-          <small className="character-name">
-            {t('register.nombre')}
-            {' '}
-            :
-          </small>
+          :
+        </small>
+        {' '}
+        {item.name.title}
+      </p>
+      <p className="character-info">
+        <small className="character-name">
+          {t('r.birthday')}
           {' '}
-          {item.name}
-        </p>
-        <p className="character-info">
-          <small className="character-name">
-            {t('register.genero')}
-            {' '}
-            :
-          </small>
-          {item.gender}
-        </p>
-      </article>
-    </Link>
+          :
+        </small>
+        <p className="character-date">{item.dob.date}</p>
+      </p>
+    </article>
   );
 }
